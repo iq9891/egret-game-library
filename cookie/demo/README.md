86931d7b5be6977e6d512f36523c26926d8829e5
李梦龙 <649076408@qq.com>
2019-09-23 10:19:41 +0800

    docs($cookie): add README.md

---
+++ cookie/demo/README.md
@@ -0,0 +1,4 @@
+# cookie 的 demo
+
+- EgretWing 4.1.6
+- Egret 5.2.28
--- cookie/demo/src/Main.ts
+++ cookie/demo/src/Main.ts
@@ -62,10 +62,6 @@ class Main extends eui.UILayer {
         this.createGameScene();
         const result = await RES.getResAsync("description_json")
         this.startAnimation(result);
-        await platform.login();
-        const userInfo = await platform.getUserInfo();
-        console.log(userInfo);
-
     }
 
     private async loadResource() {
